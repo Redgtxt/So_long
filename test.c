ff825c5785568d8e2f4d10e25d131b94f9fe2118
redgtxt <hp052635@gmail.com>
2024-08-22 23:36:30 +0100

    added sprites

--- test.c
+++ test.c
@@ -17,13 +17,7 @@ int main(void)
     mlx_loop(mlx);
 }
 */
-void	my_mlx_pixel_put(t_data *data, int x, int y, int color)
-{
-	char	*dst;
 
-	dst = data->addr + (y * data->line_length + x * (data->bits_per_pixel / 8));
-	*(unsigned int*)dst = color;
-}
 
 int	main(void)
 {
